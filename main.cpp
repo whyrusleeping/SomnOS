39881c5da3326a6e2f7f13161dd87733d2ce2267
Jeromy <jeromyj@gmail.com>
2012-10-30 10:13:30 -0700

    run all the programs

--- main.cpp
+++ main.cpp
@@ -8,22 +8,22 @@ int main()
 	char f4[] = "test4.jasm";
 	char f5[] = "test5.jasm";
 	System sys;
-	//Thread kb(f5);
+	Thread kb(f5);
 	Thread th(f);	
-	//Thread re(f2);
-	//Thread ad(f3);
-	//Thread ne(f4);
+	Thread re(f2);
+	Thread ad(f3);
+	Thread ne(f4);
 	
 	//th.SetPriority(20);
 
 	DSTAT("loading processes...");
 
 
-	//sys.LoadProcess(kb);
+	sys.LoadProcess(kb);
 	sys.LoadProcess(th);
-	//sys.LoadProcess(re);
-	//sys.LoadProcess(ad);
-	//sys.LoadProcess(ne);
+	sys.LoadProcess(re);
+	sys.LoadProcess(ad);
+	sys.LoadProcess(ne);
 	
 	//sys.Start();
 	sys.Test();
